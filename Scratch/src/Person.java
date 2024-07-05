6b9d0813cbb72acf63e2aba176225a0d23650648
Jay Rostosky <jay.rostosky@tlgcohort.com>
2024-07-05 09:18:32 -0700

    Add age() to Person (via Period.between())

--- Scratch/src/Person.java
+++ Scratch/src/Person.java
@@ -1,11 +1,12 @@
 import java.time.LocalDate;
+import java.time.Period;
 
 /*
  * An immutable class.
  *
  * That is, a class definition written in such a way that instance of it (Person objects),
  * once created, cannot have their properties changed.
- * There are simply no public methods to do so, i.e., getters, but NO setters.
+ * There are simply no public methods to do so, i.e., it has getters, but NO setters.
  */
 class Person {
     private final String name;
@@ -16,15 +17,26 @@ class Person {
         this.birthDate = birthDate;
     }
 
-    public String getName() {
+    /*
+     * Returns the person's age in whole years.
+     * This is the period of time between the birthdate and today's date.
+     * This is a "derived" property, i.e., it is "calculated" from existing data, not a new field.
+     *
+     * CUSTOMER QUESTION; what do we return if this value is < 1? (e.g., 8 months)
+     */
+    public int age() {
+        return Period.between(birthDate(), LocalDate.now()).getYears();
+    }
+
+    public String name() {
         return name;
     }
 
-    public LocalDate getBirthDate() {
+    public LocalDate birthDate() {
         return birthDate;
     }
 
     public String toString() {
-        return String.format("Person: name=%s, birthDate=%s", getName(), getBirthDate());
+        return String.format("Person: name=%s, birthDate=%s", name(), birthDate());
     }
 }
--- Scratch/src/PersonClient.java
+++ Scratch/src/PersonClient.java
@@ -5,7 +5,9 @@ class PersonClient {
     public static void main(String[] args) {
         Person p1 = new Person("Jay", LocalDate.of(1966, 12, 5));
 
+        // print the following: <name> is <age> years old
+        System.out.printf("%s is %s years old\n", p1.name(), p1.age());
 
-        System.out.println(p1);
+        System.out.println(p1);  // toString() automatically called
     }
 }
