a5ad66f137c517244c1b38bfca6d228cb5294516
Jay Rostosky <jay.rostosky@tlgcohort.com>
2024-07-09 10:29:21 -0700

    Demo of Java Modules (with module-info.java).

---
+++ Lab07.2-Client/src/module-info.java
@@ -0,0 +1,11 @@
+/*
+ * This module contains one package:
+ *  com.entertainment.client
+ *
+ * As the "client" module, it doesn't exports anything, but it DOES
+ * "requires" the other module, com.entertainment.
+ */
+module com.entertainment.client {
+    // what other *modules* does this module depend on (use)?
+    requires com.entertainment;
+}
