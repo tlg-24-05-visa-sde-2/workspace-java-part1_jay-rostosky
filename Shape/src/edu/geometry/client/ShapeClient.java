c6b68fe49c671e6639312dad88cab0fee457a535
Jay Rostosky <jay.rostosky@tlgcohort.com>
2024-07-15 07:05:04 -0700

    Create Shape[] in ShapeClient main-class

--- Shape/src/edu/geometry/client/ShapeClient.java
+++ Shape/src/edu/geometry/client/ShapeClient.java
@@ -1,8 +1,11 @@
 package edu.geometry.client;
 
+import edu.geometry.Shape;
+
 class ShapeClient {
 
     public static void main(String[] args) {
+        Shape[] shapes = new Shape[3];
 
     }
 }
